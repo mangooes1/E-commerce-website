6746da81939308fcc985a723185d02df2c73ae94
aditya <adityasuman990@gmail.com>
2024-08-02 23:52:00 +0530

    no

--- src/app/components/Query.jsx
+++ src/app/components/Query.jsx
@@ -1,45 +1,82 @@
-import React from 'react';
-import Accordion from 'react-bootstrap/Accordion';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import React from 'react';
+// import Accordion from 'react-bootstrap/Accordion';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-function FlushExample() {
+// function FlushExample() {
+//   return (
+//     <>
+//       <div className="text-center mb-4">
+//         <h1>FAQ</h1>
+//       </div>
+//       <Accordion defaultActiveKey="0" flush>
+//         <Accordion.Item eventKey="0">
+//           <Accordion.Header>Accordion Item #1</Accordion.Header>
+//           <Accordion.Body>
+//             <p style={{ color: '#007bff' }}>
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+//               minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+//               aliquip ex ea commodo consequat. Duis aute irure dolor in
+//               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+//               pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+//               culpa qui officia deserunt mollit anim id est laborum.
+//             </p>
+//           </Accordion.Body>
+//         </Accordion.Item>
+//         <Accordion.Item eventKey="1">
+//           <Accordion.Header>Accordion Item #2</Accordion.Header>
+//           <Accordion.Body>
+//             <p style={{ color: '#007bff' }}>
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+//               minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+//               aliquip ex ea commodo consequat. Duis aute irure dolor in
+//               reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+//               pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+//               culpa qui officia deserunt mollit anim id est laborum.
+//             </p>
+//           </Accordion.Body>
+//         </Accordion.Item>
+//       </Accordion>
+//     </>
+//   );
+// }
+
+// export default FlushExample;
+
+
+
+import Accordion from 'react-bootstrap/Accordion';
+
+function BasicExample() {
   return (
-    <>
-      <div className="text-center mb-4">
-        <h1>FAQ</h1>
-      </div>
-      <Accordion defaultActiveKey="0" flush>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            <p style={{ color: '#007bff' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            <p style={{ color: '#007bff' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-    </>
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   );
 }
 
-export default FlushExample;
+export default BasicExample;
